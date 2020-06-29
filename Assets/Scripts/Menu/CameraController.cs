6535d3bce24a90ed500438d0fe7dd324bdb51be5
Jean Catanho <jensmcatanho@gmail.com>
2020-06-29 02:15:46 -0300

    Add Menu intro and idle camera animation

---
+++ Assets/Scripts/Menu/CameraController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Menu {
+
+    public class CameraController : MonoBehaviour {
+
+        private Hashtable _initialMovementArgs = null;
+
+        private Hashtable _idleMovementArgs = null;
+
+        private void Awake() {
+            iTween.Init(gameObject);
+
+            _initialMovementArgs = new Hashtable {
+                { "position", new Vector3(115, 50, 40) },
+                { "easetype", iTween.EaseType.easeOutExpo },
+                { "delay", 2.0f },
+                { "time", 10.0f },
+                { "oncomplete", "IdleMovement" }
+            };
+
+            _idleMovementArgs = new Hashtable {
+                { "position", new Vector3(115, 50.25f, 40) },
+                { "easetype", iTween.EaseType.easeInOutSine },
+                { "looptype", iTween.LoopType.pingPong },
+                { "time", 2.0f }
+            };
+        }
+
+        private void Start() {
+            iTween.MoveTo(gameObject, _initialMovementArgs);
+        }
+
+        private void IdleMovement() {
+            iTween.MoveTo(gameObject, _idleMovementArgs);
+
+        }
+    }
+}
