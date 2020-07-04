78377a0fbb3d1f928fc9d4f12de88c810f8bd4e3
Jean Catanho <jensmcatanho@gmail.com>
2020-07-04 02:06:49 -0300

    Add script to handler wall destruction

--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -1,18 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Wall : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class Wall : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
---
+++ Assets/Scripts/WallDestructor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class WallDestructor : MonoBehaviour {
+
+    private Camera _playerCamera;
+
+    private float _maxDistance = 2.0f;
+
+    private void Awake() {
+        _playerCamera = GetComponentInChildren<Camera>();
+    }
+
+    void Update() {
+
+        if (Input.GetKeyDown(KeyCode.F)) {
+            var ray = _playerCamera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out RaycastHit hit, _maxDistance))
+                if (hit.transform.TryGetComponent(out Wall wall))
+                    Destroy(wall.gameObject);
+        }
+    }
+}
